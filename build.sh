3f67a8f08e0cfc3c2654f0f48b4612c7aff72ba4
ram-techie584 <rambhupalreddy2000@gmail.com>
2025-12-13 20:24:29 +0530

    Force create tables on Render using fake-initial

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+
+
+
+
+
 #!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate --run-syncdb
+python manage.py migrate --fake-initial
+python manage.py migrate
+
+python manage.py collectstatic --noinput
